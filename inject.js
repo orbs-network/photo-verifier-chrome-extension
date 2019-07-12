27accae5ec90ac3747fb6619987095db4544d73a
ymetelkin <ymetelkin@ap.org>
2019-07-12 14:12:26 -0400

    Update inject.js

--- inject.js
+++ inject.js
@@ -3,9 +3,12 @@ for (var i = 0; i < allImages.length; i++) {
   const img = allImages[i];
   chrome.runtime.sendMessage({ action: 'verify', url: img.src }, function (response) {
     if (response.ok) {
-      debugger
       img.style.border = "5px solid green";
-      img.title = JSON.stringify(response.source);
+      let title = ''
+      for (var key in response.source) {
+        title += key + ': ' + response.source[key] + '\n';
+      }
+      img.title = title;
     } else {
       img.style.border = "5px solid red";
       img.title = "WARNING! This image is in violation of Photo Registry laws. Blockchain or die!";
