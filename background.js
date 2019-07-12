43c57ddf799a1e81b452220d75d06a0ac6d897d8
ymetelkin <ymetelkin@ap.org>
2019-07-12 14:48:25 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,33 +1,43 @@
+var account = Orbs.createAccount();
+console.log("New account: " + account.publicKey);
+
+var client = new Orbs.Client('https://validator.orbs-test.com/vchains/6666', 6666, 'TEST_NET');
+
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    if (request.action == "verify") {
-      const body = '{"url":"' + request.url + '"}'
+    //console.log(request)
+    //console.log(sender)
+    //console.log(sendResponse)
 
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', 'http://10.240.2.76:5678');
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.onload = async function () {
-        if (xhr.status === 200) {
-          const phash = xhr.responseText;
-          const r = await query(phash)
-          sendResponse(r)
-        }
-        else {
-          sendResponse({ ok: false })
-        }
-      };
-      xhr.send(body);
+    try {
+      if (request.action == "verify") {
+        const body = '{"url":"' + request.url + '"}'
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://10.240.2.76:5678');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = async function () {
+          if (xhr.status === 200) {
+            const phash = xhr.responseText;
+            const r = await query(phash)
+            sendResponse(r)
+          }
+          else {
+            sendResponse({ ok: false })
+          }
+        };
+        xhr.send(body);
+      }
     }
+    catch (err) {
+      sendResponse({ ok: false })
+    }
+
     return true
   });
 
 
 async function query(phash) {
-  var account = Orbs.createAccount();
-  console.log("New account: " + account.publicKey);
-
-  var client = new Orbs.Client('https://validator.orbs-test.com/vchains/6666', 6666, 'TEST_NET');
-
   const q = client.createQuery(
     account.publicKey,
     'registry',
@@ -48,3 +58,4 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     file: "/inject.js"
   });
 });
+
