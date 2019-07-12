5e6e9408cbbeae598dd72f81d879d29a09ae4b8b
Tal Kol <talkol@gmail.com>
2019-07-12 17:00:23 +0300

    Add files via upload

--- inject.js
+++ inject.js
@@ -1,5 +1,5 @@
 var allImages = document.getElementsByTagName("img");
 for (var i = 0; i < allImages.length ; i++) {
   allImages[i].style.border = "5px solid red";
+  allImages[i].title = "Name: Some photo\nPhotographer: Some photographer\nLicense: Some license";
 }
-document.body.style.border = "5px solid green";
