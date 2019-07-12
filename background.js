2f89772f416ad5f5baca4818e5a726b05080270f
ymetelkin <ymetelkin@ap.org>
2019-07-12 13:23:25 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,52 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    file: "/orbs-client-sdk.js"
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.action == "verify") {
+      const body = '{"url":"' + request.url + '"}'
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://10.240.2.76:5678');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onload = async function () {
+        if (xhr.status === 200) {
+          const phash = xhr.responseText;
+          const r = await query(phash)
+          sendResponse(r)
+        }
+        else if (xhr.status !== 200) {
+          console.error('Request failed.  Returned status of ' + xhr.status);
+          sendResponse({ error: "ERROR" })
+        }
+      };
+      xhr.send(body);
+    }
+    return true
   });
 
+
+async function query(phash) {
+  var account = Orbs.createAccount();
+  console.log("New account: " + account.publicKey);
+
+  var client = new Orbs.Client('https://validator.orbs-test.com/vchains/6666', 6666, 'TEST_NET');
+
+  const q = client.createQuery(
+    account.publicKey,
+    'registry',
+    'verify',
+    [Orbs.argString(phash)]
+  )
+
+  const r = await client.sendQuery(q)
+  if (r.executionResult != "ERROR_SMART_CONTRACT") {
+    const jo = JSON.parse(r.outputArguments[0].value);
+    return { ok: true, source: jo }
+  }
+  return { ok: false }
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.executeScript({
     file: "/inject.js"
   });
 });
+
